dfac57951bbe9c720a5a0ed5eb749438d2e59f4a
jatin <jagdish@kunato.io>
2024-04-16 14:45:55 +0530

    iterator

--- src/second.rs
+++ src/second.rs
@@ -48,12 +48,28 @@ impl<T> Drop for List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
 
     #[test]
-    fn basic() {
+    fn generic() {
         let mut list = List::new();
 
         assert_eq!(list.peek(), None);
@@ -81,5 +97,20 @@ mod test {
 
         assert_eq!(list.pop(), Some(42));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(2);
+        list.push(4);
+        list.push(6);
+
+        let mut iter = list.into_iter();
+
+        assert_eq!(iter.next(), Some(6));
+        assert_eq!(iter.next(), Some(4));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), None)
     }
 }
